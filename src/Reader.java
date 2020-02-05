3d062124fa97afb5bef6f9d5484d7d1b612e0a76
tmurray19 <me@taidghmurray.ie>
2020-02-05 10:43:23 +0000

    Initial commit

---
+++ src/Reader.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Reader {
+    // We need to create a class that can read in text and parse it
+
+    // Define array of strings
+    private ArrayList<String> t;
+
+
+    public void splitter(String text){
+
+    }
+
+}
