d496eca8df61df708f1c46018dcaf215442b10cc
tmurray19 <me@taidghmurray.ie>
2020-02-12 15:46:49 +0000

    Updating ALICE bot

--- src/Chatbot.java
+++ src/Chatbot.java
@@ -7,40 +7,54 @@ import org.alicebot.ab.MagicStrings;
 import org.alicebot.ab.utils.IOUtils;
 
 public class Chatbot {
-    private static final boolean TRACE_MODE = false;
-    static String botName = "super";
+    // TRACE_MODE set to true for debugging
+    // false for production
+    private static final boolean TRACE_MODE = true;
+    static String botName = "Super";
 
     public static void main(String[] args) {
         try {
 
             String resourcesPath = getResourcesPath();
-            System.out.println(resourcesPath);
+            System.out.println("Reource path: " + resourcesPath);
+            // TRACE_MODE toggles a debugging mode
             MagicBooleans.trace_mode = TRACE_MODE;
-            Bot bot = new Bot("super", resourcesPath);
+            // Create the bot from the AB.jar
+            Bot bot = new Bot(botName, resourcesPath);
+            // Create a new ChatSession with the bot
             Chat chatSession = new Chat(bot);
             bot.brain.nodeStats();
             String textLine = "";
 
             while(true) {
+                // Read the text in from the human
                 System.out.print("Human : ");
+                // An IO function that reads in the input from a user
                 textLine = IOUtils.readInputTextLine();
+                // Null input
                 if ((textLine == null) || (textLine.length() < 1))
                     textLine = MagicStrings.null_input;
+                // Quits bot
                 if (textLine.equals("q")) {
                     System.exit(0);
-                } else if (textLine.equals("wq")) {
+                }
+                // Quits input with writing
+                else if (textLine.equals("wq")) {
                     bot.writeQuit();
                     System.exit(0);
-                } else {
+                }
+                else {
                     String request = textLine;
+                    // For debugging
                     if (MagicBooleans.trace_mode)
                         System.out.println("STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0) + ":TOPIC=" + chatSession.predicates.get("topic"));
+                    // Generates a response from the user
                     String response = chatSession.multisentenceRespond(request);
                     while (response.contains("&lt;"))
                         response = response.replace("&lt;", "<");
                     while (response.contains("&gt;"))
                         response = response.replace("&gt;", ">");
-                    System.out.println("Robot : " + response);
+                    System.out.println(botName + " : " + response);
                 }
             }
         } catch (Exception e) {
@@ -48,6 +62,8 @@ public class Chatbot {
         }
     }
 
+    // This function just returns the resources folder for the bot
+    // It contextually determines it based on the operating system.
     private static String getResourcesPath() {
         File currDir = new File(".");
         String path = currDir.getAbsolutePath();
